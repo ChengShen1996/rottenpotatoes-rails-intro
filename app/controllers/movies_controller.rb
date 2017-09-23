c8840b8d0516253da22688e3f754cc35925872f2
ChengShen1996 <617984532@qq.com>
2017-09-22 22:17:45 -0700

    dssdfds

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -32,11 +32,11 @@ class MoviesController < ApplicationController
       redirect_to :sort_term => val_sort, :val_rating => @val_rating and return
     end
 
-  # if params[:ratings] != session[:ratings] and @val_rating != {}
-  #     session[:sort_term] = val_sort
-  #     session[:ratings] = @val_rating
-  #     redirect_to val_sort => val_sort, val_rating => @val_rating and return
-  #   end
+  if params[:ratings] != session[:ratings] and @val_rating != {}
+      session[:sort_term] = val_sort
+      session[:ratings] = @val_rating
+      redirect_to :sort_term => val_sort, :val_rating => @val_rating and return
+    end
 
 
 
